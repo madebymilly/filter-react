af4f9fe210906c081d9f9235eafdfb53cabfc7dc
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2019-06-30 13:40:43 +0200

    added content of filteritems to static version in react

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,6 +4,37 @@ import ResetFilterBtn from './ResetFilterBtn'
 import FilterGroups from './FilterGroups'
 import ShowResultsBtn from './ShowResultsBtn'
 
+const FILTERITEMS = [
+  {
+    group: 'group1',
+    label: 'genre',
+    items: [
+      'Dans', 'Kunst & Design', 'Muziek', 'Theater'
+    ]
+  },
+  {
+    group: 'group2',
+    label: 'doelgroep',
+    items: [
+      'Allerkleinsten', 'Kinderen', 'Jongeren', 'Volwassenen', 'Senioren'
+    ]
+  },
+  {
+    group: 'group3',
+    label: 'dag',
+    items: [
+      'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'
+    ]
+  },
+  {
+    group: 'group4',
+    label: 'locatie',
+    items: [
+      'Dordrect', 'Papendrecht', 'Ridderkerk', 'Zwijndrecht'
+    ]
+  },
+];
+
 class Filter extends React.Component {
   render() {
     return(
@@ -16,7 +47,7 @@ class Filter extends React.Component {
 
       	<div className="filter-scroll">
       		<div className="filter-inner">
-            <FilterGroups />
+            <FilterGroups groups={FILTERITEMS} />
       		</div>
       	</div>
 
--- src/components/FilterItem.js
+++ src/components/FilterItem.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
 class FilterItem extends React.Component {
+
   render() {
+
+    const item = this.props.item
+
     return(
-      <div>item</div>
+      <div className="filter-item js-filter-item" data-filter={item} data-filter-group={this.props.group}>
+        <label>
+          <span className="checkbox"><span className="check"></span></span>
+          <span className="text">{item}  <em></em></span>
+        </label>
+      </div>
     )
   }
 }
