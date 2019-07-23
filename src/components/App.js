6952185ac2e333ccaff80cca76491f670a9ff2f1
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2019-07-23 15:09:24 +0200

    fetch courses from json

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,34 @@ import FilterableCourses from './FilterableCourses'
 import Course from './Course'
 import ResetFilterBtn from './ResetFilterBtn'
 
-const COURSES = [
-  {
-    "post_id":40,
-    "title":"Jeugdtoneel Brugklas",
-    "url":"http://oscu.test/cursus/jeugdtoneel-brugklas/",
-    "image":"http://oscu.test/content/uploads/2019/04/1920x1080-1-300x200.jpg",
-    "image_alt":"",
-    "groups":{
-      "group1":["Theater"],
-      "group2":["Jongeren"],
-      "group3":["Woensdag","Vrijdag"],
-      "group4":["Dordrecht","Papendrecht","Ridderkerk","Zwijndrecht"]
-    }
-  },
-  {
-    "post_id":38,
-    "title":"Moderne dans voor beginners",
-    "url":"http://oscu.test/cursus/moderne-dans-voor-beginners/",
-    "image":"http://oscu.test/content/uploads/2019/04/alex-klaasen-showponies-1-2-300x200.jpg",
-    "image_alt":"",
-    "groups":{
-      "group1":["Dans"],
-      "group2":["Kinderen","Jongeren"],
-      "group3":["Maandag","Woensdag","Vrijdag"],
-      "group4":["Dordrecht","Papendrecht","Ridderkerk","Zwijndrecht"]
-    }
-  }
-];
+// const COURSES = [
+//   {
+//     "post_id":40,
+//     "title":"Jeugdtoneel Brugklas",
+//     "url":"http://oscu.test/cursus/jeugdtoneel-brugklas/",
+//     "image":"http://oscu.test/content/uploads/2019/04/1920x1080-1-300x200.jpg",
+//     "image_alt":"",
+//     "groups":{
+//       "group1":["Theater"],
+//       "group2":["Jongeren"],
+//       "group3":["Woensdag","Vrijdag"],
+//       "group4":["Dordrecht","Papendrecht","Ridderkerk","Zwijndrecht"]
+//     }
+//   },
+//   {
+//     "post_id":38,
+//     "title":"Moderne dans voor beginners",
+//     "url":"http://oscu.test/cursus/moderne-dans-voor-beginners/",
+//     "image":"http://oscu.test/content/uploads/2019/04/alex-klaasen-showponies-1-2-300x200.jpg",
+//     "image_alt":"",
+//     "groups":{
+//       "group1":["Dans"],
+//       "group2":["Kinderen","Jongeren"],
+//       "group3":["Maandag","Woensdag","Vrijdag"],
+//       "group4":["Dordrecht","Papendrecht","Ridderkerk","Zwijndrecht"]
+//     }
+//   }
+// ];
 
 // function openFilter() {
 // 	if ( ! filter.$this.hasClass( 'is-open' ) ) {
@@ -69,6 +69,8 @@ class App extends React.Component {
     this.state = {
       isFilterOpen: false,
       activeItems: [],
+      allCourses: [],
+      isLoading: false,
     }
     this.toggleFilter = this.toggleFilter.bind(this)
 
@@ -76,6 +78,21 @@ class App extends React.Component {
     this.close = this.close.bind(this)
     this.handleActivatedFilterItems = this.handleActivatedFilterItems.bind(this)
   }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+		fetch('../data/courses.json')
+			.then(data => data.json())
+      .then(result => {
+        const allCourses = result.map( course => {
+          return course;
+        } )
+        this.setState( {
+          allCourses: allCourses,
+          isLoading: false
+        } )
+      })
+	}
 
   toggleFilter() {
     this.setState( prevState => ({
@@ -99,7 +116,7 @@ class App extends React.Component {
     //console.log(item)
     //console.log(this.state.activeItems)
 
-// opsplitsen in add & remove!
+    // TODO: opsplitsen in add & remove!
 
     let tempArray = this.state.activeItems
     tempArray.unshift(item)
@@ -111,7 +128,7 @@ class App extends React.Component {
 
   render() {
 
-    const courses = COURSES;
+    const courses = this.state.allCourses;
 
     return(
       <div>
@@ -134,7 +151,7 @@ class App extends React.Component {
     		</div>
         <Filter close={this.close} shown={this.state.isFilterOpen} activateFilterItem={this.handleActivatedFilterItems} />
         <div className="center">
-    			<div className="filter-loader js-filter-loader lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    			<div className={`filter-loader js-filter-loader lds-spinner ${this.state.isLoading ? '' : 'is-hidden'}`}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
           <FilterableCourses>
             {courses.map(
               (course, i) =>
--- src/components/FilterableCourses.js
+++ src/components/FilterableCourses.js
@@ -1,21 +1,4 @@
 import React, { Component } from 'react'
-// import Course from './Course'
-
-// class FilterableCourses extends React.Component {
-//   render() {
-//
-//     const courses = this.props.courses;
-//
-//     return(
-//       <div id="courses" className="course-items">
-//         {courses.map(
-//           (course, i) =>
-//             <Course key={i} course={course} />
-//         )}
-//       </div>
-//     )
-//   }
-// }
 
 const FilterableCourses = ({ children }) =>
   <div>
