965763f0ff8e332977b140bf2273c66132823b28
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2019-10-21 10:52:01 +0200

    cleaned up code, based on WordPress-ESLint

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 // @flow
-import React, { Component } from 'react'
-import Filter from './Filter'
-import ResultNumber from './ResultNumber'
-import OpenFilterBtn from './OpenFilterBtn'
-import Course from './Course'
-import ResetFilterBtn from './ResetFilterBtn'
+import React, { Component } from 'react';
+import Filter from './Filter';
+import ResultNumber from './ResultNumber';
+import OpenFilterBtn from './OpenFilterBtn';
+import Course from './Course';
+import ResetFilterBtn from './ResetFilterBtn';
 
-import { filterItems } from '../../data/filterItems.js'
+import { filterItems } from '../../data/filterItems.js';
 
 let scrollTop = 0;
 let activeItems = {}; // Geen state, want kan berekend worden op basis van filterItem state.
@@ -16,69 +16,68 @@ const container = document.querySelector( '.js-container' ),
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props );
     this.state = {
       isFilterOpen: false,
       allCourses: [],
       filteredCourses: [],
       filterItems: filterItems,
-      isLoading: false,
-      // resultNumber is geen state, want kan berekend worden met state.filteredCourses.length
-    }
+      isLoading: false
 
-    this.toggleFilter = this.toggleFilter.bind(this)
-    this.open = this.open.bind(this)
-    this.close = this.close.bind(this)
-    this.reset = this.reset.bind(this)
-    this.getActivatedFilterItems = this.getActivatedFilterItems.bind(this)
-    this.activateFilterItem = this.activateFilterItem.bind(this)
-    this.getShowGroups = this.getShowGroups.bind(this)
-    this.getResultNumber = this.getResultNumber.bind(this)
-    this.updateCourses = this.updateCourses.bind(this)
+      // resultNumber is geen state, want kan berekend worden met state.filteredCourses.length
+    };
+    this.toggleFilter = this.toggleFilter.bind( this );
+    this.open = this.open.bind( this );
+    this.close = this.close.bind( this );
+    this.reset = this.reset.bind( this );
+    this.getActivatedFilterItems = this.getActivatedFilterItems.bind( this );
+    this.activateFilterItem = this.activateFilterItem.bind( this );
+    this.getShowGroups = this.getShowGroups.bind( this );
+    this.getResultNumber = this.getResultNumber.bind( this );
+    this.updateCourses = this.updateCourses.bind( this );
   }
 
   componentDidMount() {
     this.setState({ isLoading: true });
-		fetch('../data/courses.json')
-			.then(data => data.json())
-      .then(result => {
+		fetch( '../data/courses.json' )
+			.then( data => data.json() )
+      .then( result => {
         const allCourses = result.map( course => {
           return course;
-        } )
-        this.setState( {
+        });
+        this.setState({
           allCourses: allCourses,
           filteredCourses: allCourses,
           isLoading: false
-        } )
-      })
+        });
+      });
 	}
 
   toggleFilter() {
     this.setState( prevState => ({
-      isFilterOpen: !prevState.isFilterOpen
-    }))
+      isFilterOpen: ! prevState.isFilterOpen
+    }) );
 	}
 
   open( e ) {
-    e.preventDefault()
-    this.toggleFilter()
+    e.preventDefault();
+    this.toggleFilter();
 
     scrollTop = window.pageYOffset;
 
     document.body.classList.add( 'fixed' );
     container.classList.add( 'fixed' );
-    container.style.top = -Math.abs( scrollTop ) + "px";
-
+    container.style.top = -Math.abs( scrollTop ) + 'px';
   }
 
   close( e ) {
-    e.preventDefault()
-    this.toggleFilter()
+    e.preventDefault();
+    this.toggleFilter();
 
-    document.body.classList.remove('fixed');
+    document.body.classList.remove( 'fixed' );
     container.classList.remove( 'fixed' );
-    container.style.top = "0px";
+    container.style.top = '0px';
   }
 
   reset( e ) {
@@ -87,23 +86,22 @@ class App extends React.Component {
     const tempFilterItems = this.state.filterItems;
     for ( let group in tempFilterItems ) { // TODO: map & arrow function gebruiken
       for ( let item in tempFilterItems[group].items ) {
-        tempFilterItems[group].items[item].active = false
+        tempFilterItems[group].items[item].active = false;
       }
     }
-    this.updateCourses( activeItems = {}, tempFilterItems )
+    this.updateCourses( activeItems = {}, tempFilterItems );
   }
 
   getActivatedFilterItems( item, group, activeState ) {
     if ( activeState ) {
       if ( activeItems[group] == undefined ) {
-    		activeItems[group] = [];
-    	}
-    	activeItems[group].push( item );
-
+        activeItems[group] = [];
+      }
+      activeItems[group].push( item );
     } else {
-      var index = activeItems[group].indexOf( item );
-      if ( index > -1 ) {
-        activeItems[group].splice(index, 1);
+      const index = activeItems[group].indexOf( item );
+      if ( -1 < index ) {
+        activeItems[group].splice( index, 1 );
       }
     }
     return activeItems;
@@ -119,18 +117,18 @@ class App extends React.Component {
 
     for ( let group in tempFilterItems ) { // TODO: map & arrow function gebruiken
       for ( let item in tempFilterItems[group].items ) {
-        if ( tempFilterItems[group].group == itemGroup) {
-          if ( tempFilterItems[group].items[item].value == itemValue) {
+        if ( tempFilterItems[group].group == itemGroup ) {
+          if ( tempFilterItems[group].items[item].value == itemValue ) {
 
             // add to or remove from 'activeItems':
             // TODO: deze functionaliteit kan ook in updateCourses komen. dan hoef je niet 2x 'activeItems' door te geven.
-            activeItems = this.getActivatedFilterItems( itemValue, itemGroup, !tempFilterItems[group].items[item].active );
+            activeItems = this.getActivatedFilterItems( itemValue, itemGroup, ! tempFilterItems[group].items[item].active );
 
             // change state:
-            tempFilterItems[group].items[item].active = !tempFilterItems[group].items[item].active
+            tempFilterItems[group].items[item].active = ! tempFilterItems[group].items[item].active;
 
             // update courses:
-            this.updateCourses( activeItems, tempFilterItems )
+            this.updateCourses( activeItems, tempFilterItems );
           }
         }
       }
@@ -138,19 +136,19 @@ class App extends React.Component {
   }
 
   getShowGroups( course, group, activeItems ) {
-  	var show_group = false;
-  	activeItems[ group ].forEach( function( item ) {
-  		if (course.groups[ group ].includes( item )) {
-  			if (!show_group) {
-  				show_group = course.groups[ group ].includes( item );
-  			}
-  		}
-  	} );
-  	return show_group;
+    var showGroup = false;
+    activeItems[ group ].forEach( function( item ) {
+      if ( course.groups[ group ].includes( item ) ) {
+        if ( ! showGroup ) {
+          showGroup = course.groups[ group ].includes( item );
+        }
+      }
+      });
+      return showGroup;
   }
 
   getResultNumber() {
-    return this.state.filteredCourses.length
+    return this.state.filteredCourses.length;
   }
 
   updateCourses( activeItems, tempFilterItems ) {
@@ -158,50 +156,51 @@ class App extends React.Component {
     // alleen deze functie mag state updaten ivm met asyncronous state changes.
     // (geldt alleen voor states waar verdere berekeningen / statussen mee gemaakt worden)
 
-    const this_app = this;
+    const _this = this;
     const allCourses = this.state.allCourses;
 
     let tempFilteredCourses = [];
-    let count_results = 0;
+    let countResults = 0;
 
     this.setState({ isLoading: true });
 
     // === if noFilters are selected ===
-  	let noFilters = Object.entries( activeItems ).length === 0;
+    let noFilters = 0 === Object.entries( activeItems ).length;
 
     if ( noFilters ) {
 
       allCourses.forEach( function( course ) {
-        tempFilteredCourses.push( course )
-        count_results++;
-      } );
+        tempFilteredCourses.push( course );
+        countResults++;
+      });
 
     // === if filters are selected ===
     } else {
 
       allCourses.forEach( function( course ) {
 
-        let show_groups = [];
+        let showGroups = [];
+        var i;
 
-    		for ( var i = 1; i <= numberOfGroups; i ++ ) {
+        for ( i = 1; i <= numberOfGroups; i++ ) {
 
-          let show_group = true;
-    		  let group = 'group' + i;
+          let showGroup = true;
+          let group = 'group' + i;
 
-    			if (course.groups[group] && course.groups[group].length > 0 && activeItems[group] && activeItems[group].length > 0) {
-    				show_group = this_app.getShowGroups( course, group, activeItems );
-    			}
-    			show_groups.push(show_group);
+          if ( course.groups[group] && 0 < course.groups[group].length && activeItems[group] && 0 < activeItems[group].length ) {
+            showGroup = _this.getShowGroups( course, group, activeItems );
+          }
+          showGroups.push( showGroup );
         }
 
-        let itemShouldBeShown = show_groups.every( el => el === true );
+        let itemShouldBeShown = showGroups.every( el => true === el );
 
-  			if ( itemShouldBeShown ) {
-          tempFilteredCourses.push( course )
-  				count_results++;
-  			}
+        if ( itemShouldBeShown ) {
+          tempFilteredCourses.push( course );
+          countResults++;
+        }
 
-      } );
+      });
 
     }
 
@@ -209,7 +208,7 @@ class App extends React.Component {
       filteredCourses: tempFilteredCourses,
       isLoading: false,
       filterItems: tempFilterItems
-    })
+    });
 
   }
 
@@ -219,38 +218,38 @@ class App extends React.Component {
       filterItems = this.state.filterItems,
       resultNumber = this.getResultNumber();
 
-    return(
+    return (
       <div>
         <div className="results">
-    			<div className="center">
-    				<ResultNumber number={resultNumber} />
-    				<OpenFilterBtn open={this.open} />
-    			</div>
-    		</div>
+          <div className="center">
+            <ResultNumber number={resultNumber} />
+            <OpenFilterBtn open={this.open} />
+          </div>
+        </div>
         <div className="center">
-    			<div className="filter-extra">
-    				<a href="#">Dans</a>
-    				<a href="#">Dordrecht</a>
-    				<a href="#">Modern</a>
-    				<a href="#">Dansfit</a>
-    				<a href="#">Volwassenen</a>
-    				<a href="#">Middag</a>
-    				<ResetFilterBtn reset={this.reset} />
-    			</div>
-    		</div>
+          <div className="filter-extra">
+            <a href="#">Dans</a>
+            <a href="#">Dordrecht</a>
+            <a href="#">Modern</a>
+            <a href="#">Dansfit</a>
+            <a href="#">Volwassenen</a>
+            <a href="#">Middag</a>
+            <ResetFilterBtn reset={this.reset} />
+          </div>
+        </div>
         <Filter close={this.close} shown={this.state.isFilterOpen} reset={this.reset} activateFilterItem={this.activateFilterItem} filterItems={filterItems} resultNumber={resultNumber} />
         <div className="center">
-    			<div className={`filter-loader js-filter-loader lds-spinner ${this.state.isLoading ? '' : 'is-hidden'}`}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-          <p className={`no-results ${resultNumber !== 0 ? 'is-hidden' : ''}`}>Er zijn geen resultaten gevonden.</p>
+          <div className={`filter-loader js-filter-loader lds-spinner ${this.state.isLoading ? '' : 'is-hidden'}`}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          <p className={`no-results ${0 !== resultNumber ? 'is-hidden' : ''}`}>Er zijn geen resultaten gevonden.</p>
           <div className="course-items">
             {courses.map(
-              (course, i) =>
+              ( course, i ) =>
                 <Course key={i} course={course} />
             )}
           </div>
-    		</div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
