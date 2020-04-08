4179c7a9058b010838d996e51878fa7a2aee2948
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2020-04-08 08:24:00 +0200

    added if statement to check if courses-archive exists

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-ReactDOM.render(
-	<App />,
-  document.getElementById('courses-archive')
-);
+if (document.getElementById('courses-archive') !== null) {
+
+	ReactDOM.render(
+		<App />,
+		document.getElementById('courses-archive')
+	);
+
+}
